17415079ca55feaed773af668cc5b2470b962386
developerNoman <noman.shafique@rakurai.io>
2024-07-11 15:09:41 +0500

    updating the remote repo

--- tasks/task5/task5.sh
+++ tasks/task5/task5.sh
@@ -11,12 +11,30 @@
    #!/bin/bash
 
 
-read -p "Enter the Path: " r1
+# read -p "Enter the Path: " r1
 # read -p "Please Enter the file to update: " r2
 # filePath="${r1}/${r2}"
 
-git add .
+# git add "$filePath"
 
+# git commit -m "updating the remote repo"
+
+# git push origin main
+
+
+#!/bin/bash
+
+# read -p "Enter the Path: " r1
+read -p "Please Enter the file to update: " r2
+
+# Navigate to the specified path
+# cd "$r1" || exit
+
+# Add the specified file to the staging area
+git add "$r2"
+
+# Commit the changes with a message
 git commit -m "updating the remote repo"
 
+# Push the changes to the main branch
 git push origin main
