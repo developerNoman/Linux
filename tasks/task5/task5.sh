43e7eeeeb81817dd3650cbaee19ef11f5e04de05
developerNoman <noman.shafique@rakurai.io>
2024-07-11 13:53:09 +0500

    updating the remote repo

--- tasks/task5/task5.sh
+++ tasks/task5/task5.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
-cd /Linux/task/task5
+# #!/bin/bash
+# cd /Linux/task/task5
 # git_status=$(git status --porcelain)
 # if [ ! -z "$git_status" ]; then
-    git add .
-    git commit -m "updating the remote repo"
-    git push origin main
-    echo "Changes have been pushed to the remote repository."
+#     git add .
+#     git commit -m "updating the remote repo"
+#     git push origin main
+#     echo "Changes have been pushed to the remote repository."
 # else
 #     echo "No changes to commit."
 # fi
+
+
+#!/bin/bash
+cd /Linux/task/task5
+read -p "Please Enter Path:" $r1
+ git add .
+git commit -m "updating the remote repo"
+git push origin main
+   
