0e6d4179cbe8af2435686ea27b4246624fb46033
developerNoman <noman.shafique@rakurai.io>
2024-07-11 16:56:48 +0500

    updating the remote repo

--- tasks/task5/task5.sh
+++ tasks/task5/task5.sh
@@ -4,16 +4,34 @@
 # cd "$r1" || exit
 # git status
 
+# git_status=$(git status --porcelain)
+
+# echo "$git_status"
+# read -p "Enter the file to update: " r2
+# if [ ! -z "$git_status" ]; then
+# git add "$r2"
+# git commit -m "updating the remote repo"
+# git push origin main
+# echo "Changes have been pushed to the remote repository."
+# else
+#     echo "No changes to commit."
+# fi
+
+
+#!/bin/bash
+
 git_status=$(git status --porcelain)
 
-echo "$git_status"
-read -p "Enter the file to update: " r2
-# If there are changes, add, commit, and push
 if [ ! -z "$git_status" ]; then
-git add "$r2"
-git commit -m "updating the remote repo"
-git push origin main
-echo "Changes have been pushed to the remote repository."
+    echo "$git_status"
+    read -p "Enter the files to update (separated by spaces): " -a files
+    for file in "${files[@]}"
+    do
+        git add "$file"
+    done
+    git commit -m "updating the remote repo"
+    git push origin main
+    echo "Changes have been pushed to the remote repository."
 else
     echo "No changes to commit."
 fi
