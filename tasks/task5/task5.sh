db4fdee59df049e03f1ebdc0a629e747e558cc16
developerNoman <noman.shafique@rakurai.io>
2024-07-11 14:56:56 +0500

    updating the remote repository

--- tasks/task5/task5.sh
+++ tasks/task5/task5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # cd Linux/tasks/task5
-read -p "Enter the Path:" $r1                      #the -p is used to read the open file descriptor
+read -p "Path: " $r1                      #the -p is used to read the open file descriptor
 git add .
-git commit -m "updating the remote repo"
+git commit -m "updating the remote repository"
 git push origin main
    
 
