5eec083c1e217a9d3336971810ab9d5bc5964c0c
developerNoman <noman.shafique@rakurai.io>
2024-07-11 17:02:18 +0500

    hello, this is up to date commit

--- tasks/task5/task5.sh
+++ tasks/task5/task5.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
 
-# read -p "Enter the Path: " r1
-# cd "$r1" || exit
-# git status
-
-# git_status=$(git status --porcelain)
-
-# echo "$git_status"
-# read -p "Enter the file to update: " r2
-# if [ ! -z "$git_status" ]; then
-# git add "$r2"
-# git commit -m "updating the remote repo"
-# git push origin main
-# echo "Changes have been pushed to the remote repository."
-# else
-#     echo "No changes to commit."
-# fi
-
-
-#!/bin/bash
-
 git_status=$(git status --porcelain)
 
 if [ ! -z "$git_status" ]; then
     echo "$git_status"
-    read -p "Enter the files to update (separated by spaces): " -a files
+    read -p "Enter the files to update: " -a files
     for file in "${files[@]}"
     do
         git add "$file"
     done
-    git commit -m "updating the remote repo"
+    read -p "enter the commit message" r1
+    git commit -m "$r1"
     git push origin main
     echo "Changes have been pushed to the remote repository."
 else
