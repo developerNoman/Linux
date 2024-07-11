477cfba5a6129e3f0ccea392e247904a430e3b62
developerNoman <noman.shafique@rakurai.io>
2024-07-11 14:52:50 +0500

    updating the remote repo

--- tasks/task5/task5.sh
+++ tasks/task5/task5.sh
@@ -1,19 +1,31 @@
-# #!/bin/bash
-# cd /Linux/task/task5
-# git_status=$(git status --porcelain)
-# if [ ! -z "$git_status" ]; then
-#     git add .
-#     git commit -m "updating the remote repo"
-#     git push origin main
-#     echo "Changes have been pushed to the remote repository."
-# else
-#     echo "No changes to commit."
-# fi
-
-
 #!/bin/bash
-read -p "Please Enter Path:" $r1
- git add .
+# cd Linux/tasks/task5
+read -p "Enter the Path:" $r1                      #the -p is used to read the open file descriptor
+# read -p "Please Enter the file to update:" $r2
+# hhhh=$(readlink "${r1}")
+
+
+git add .
 git commit -m "updating the remote repo"
 git push origin main
    
+
+#    #!/bin/bash
+
+# # Prompt the user for the path to the file
+# read -p "Enter the Path: " r1
+
+# # Prompt the user for the specific file to update within that path
+# read -p "Please Enter the file to update: " r2
+
+# # Combine the path and the file name
+# file_path="${r1}/${r2}"
+
+# # Add the specified file to the staging area
+# git add "$file_path"
+
+# # Commit the change with a message
+# git commit -m "updating the remote repo"
+
+# # Push the changes to the main branch
+# git push origin main
