538134211514b5b8a728f8e7dd4dbd62305baf28
developerNoman <noman.shafique@rakurai.io>
2024-07-11 15:14:07 +0500

    updating the remote repo

--- tasks/task5/task5.sh
+++ tasks/task5/task5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # read -p "Enter the Path: " r1
-read -p "Please Enter the file to update: " r2
+read -p "Enter the file to update: " r2
 # cd "$r1" || exit
 git add "$r2"
 git commit -m "updating the remote repo"
