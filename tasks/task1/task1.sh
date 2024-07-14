5525439a3ec8b1fd0f6bf3bfee3070fb7fb43307
developerNoman <noman.shafique@rakurai.io>
2024-07-14 16:33:25 +0500

    the task is updated with the provided feedback

--- tasks/task1/task1.sh
+++ tasks/task1/task1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-source="/home/noman/work/work1"
-destination="/home/noman/Linux/task1"
+#source="/home/noman/work/work1"
+#destination="/home/noman/Linux/task1"
+
+read -p "Enter the source directory: " source
+read -p "Enter the destination directory: " destination
 
 cp -r "$source" "$destination"
 archiveName="work1.tar.gz"
