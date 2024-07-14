e35f6f75c8fca897aaa6bcceb9a14f3504601731
developerNoman <noman.shafique@rakurai.io>
2024-07-14 16:40:02 +0500

    the task is updated with the provided feedback

--- tasks/task1/task1.sh
+++ tasks/task1/task1.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-source="/home/noman/work/work1"
-destination="/home/noman/Linux/task1"
+read -p "Enter the source path: " source
+read -p "Enter the destination path: " destination
 
 cp -r "$source" "$destination"
 archiveName="work1.tar.gz"
 archiveFolder="work1"
-
-echo "Press 1 to archive or any other key to continue"
+echo "Press 1 to archive or other key to continue"
 read option
 if [ "$option" = 1 ]; then
     tar -czf "$destination/$archiveName" -C "$destination" "$archiveFolder"
